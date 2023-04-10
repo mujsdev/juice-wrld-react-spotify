77f1ec3d37c3a906bb2fd77afb606f9c72e29812
Muj <mujtmurt@gmail.com>
2023-04-10 17:34:33 +0400

    update supabase config

--- src/auth/supabase.ts
+++ src/auth/supabase.ts
@@ -2,11 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL ?? process.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY ?? process.env.VITE_SUPABASE_ANON_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: true,
-  },
-});
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const signInWithSpotify = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
